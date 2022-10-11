ba3e1b3fb4d0969337f556e34a2b04f833d44e92
Swiiptz <milan.etudes@gmail.com>
2022-10-11 11:07:20 +0200

    ee

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 carte = open("carte.txt")
 
 
-
+class Jeu:
+    def __init__(self):
+        self.grotte = [[Case(caractere) for caractere in ligne if caractere !='\n'] for ligne in carte.readlines()]
+        
+            
+    def affiche(self):
+        for n_lemming in self.grotte:
+            if n_lemming == Lemmings:
+                print("x")
+                continue
+            print(n_lemming)
 class Lemmings :
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+    def __init__(self,direction):
+        self.l = 0
+        self.c = 1
+        self.d = direction
+        self.jeu = Jeu
+    def __str__(self):
+        if self.d == 1:
+            return ">"
+        if self.d == -1:
+            return "<"
+    def action(self):
+        if self.jeu[self.l,self]:
 
+            self.l +=self.d
+    def sort(self):
+        pass
+
+
+        
 
 class Case :
     def __init__(self,caractere):
@@ -14,9 +39,5 @@ class Case :
     
 
 
-
-class Jeu :
-    def __init__(self,grotte,lemmings):
-        self.grotte = [[Case(caractere) for caractere in ligne if caractere !='\n'] for ligne in carte.readlines()]
 
 print([[Case(caractere) for caractere in ligne if caractere !='\n'] for ligne in carte.readlines()])
