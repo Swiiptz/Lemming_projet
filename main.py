5b1a98a6bdebc2a43a41c6d61622a5a8d430ef83
Thblink1995 <thblink1995@gmail.com>
2022-10-11 22:10:45 +0200

    classe jeu fct tour finie

--- main.py
+++ main.py
@@ -25,15 +25,19 @@ class Case :
     def __init__(self,caractere:str):
         self.type = caractere
         self.caractere = self.type
+
     def __str__(self):
         return str(self.caractere)
+
     def libre(self):
-        if self.type == " " :
+        if self.type == " " or "O":
             return True 
         else :
             return False
+
     def arrivee(self, lem:Lemmings):
         self.caractere = str(lem)
+
     def depart(self) :
         self.caractere = self.type
 
@@ -41,16 +45,33 @@ class Jeu:
     def __init__(self):
         self.grotte = [[Case(caractere) for caractere in ligne if caractere !='\n'] for ligne in carte.readlines()]
         self.lemmings = []
+
     def affiche(self):
         for n_lemming in self.grotte:
             if n_lemming == Lemmings:
-                print("x")
+                if n_lemming.direction == 1 :
+                    print(">")
+                else :
+                    print("<")
                 continue
             print(n_lemming)
+
     def tour(self):
-        tkt = self.lemmings
-        for i in tkt :
-            pass #en chantier
+        grotte = self.grotte
+        for i in self.lemmings :
+            l = i.l # on get les coos du lemming
+            c = i.c
+            if grotte[l-1][c].libre() == True : #check si la case du dessous est libre, si oui descendre
+                i.l -= 1
+            elif grotte[l][c+1].libre() == True and i.direction == 1 : #si libre à droite et direction droite, aller à droite
+                i.c += 1
+            elif grotte[l][c-1].libre() == True and i.direction == -1 : #pareil mais à gauche
+                i.c -= 1
+            else :
+                i.direction *= -1 #sinon change de direction
+            if grotte[i.l][i.c].type == "O" : #si le lemming est arrivé à la sortie, on le dégage
+                del i
+        self.affiche()
             
 
 
