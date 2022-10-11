21c8eba27ef7f378d8b60f39f12dfc23fb867a4d
Thblink1995 <90257853+Thblink1995@users.noreply.github.com>
2022-10-11 10:46:21 +0200

    classe case finie

--- main.py
+++ main.py
@@ -7,36 +7,30 @@ class Lemmings :
         self.x = x
         self.y = y
         liste_lemmings.append(self)
-
+    def 
 
 class Case :
-<<<<<<< HEAD
-    def __init__(self,caractere:str):
-=======
-<<<<<<< HEAD
-    def __init__(self,x,y,grotte,liste_lemmings):
-        self.terrain = grotte[x][y]
-        for i in liste_lemmings :
-            if x == i.x and y == i.y :
-                self.lemming = i
-            else :
-                self.lemming = None 
->>>>>>> 330cba59b5707f313a098d4aa6d5ab34a6d7130f
-        
-=======
     def __init__(self,caractere):
         self.type = caractere
+    def libre(self):
+        if self.type == " " :
+            return True 
+        else :
+            return False
+    def arrivee(self, lem):
+        self.caractere = lem.str()
+    def depart(self) :
+        self.caractere = self.type()
+
     
->>>>>>> dev_S
+
 
 
 class Jeu :
     def __init__(self,grotte,lemmings):
         self.grotte = [[Case(caractere) for caractere in ligne if caractere !='\n'] for ligne in carte.readlines()]
 
-<<<<<<< HEAD
 
-print(grotte = [[Case(caractere) for caractere in ligne if caractere !='\n'] for ligne in carte.readlines()])
-=======
+
 print([[Case(caractere) for caractere in ligne if caractere !='\n'] for ligne in carte.readlines()])
->>>>>>> dev_S
+
