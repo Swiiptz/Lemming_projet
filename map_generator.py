621b4f789653a56e310dbd2fcbb4569bc7a197e7
Thblink1995 <90257853+Thblink1995@users.noreply.github.com>
2022-10-20 09:07:23 +0200

    map generator v1

---
+++ map_generator.py
@@ -0,0 +1,26 @@
+from random import randint
+
+def tkt(longueur:int, hauteur:int):
+    tab_l = ["#"*longueur]
+    longueur-=2
+    tab = []
+    tab_l = []
+    tab.append(tab_l)
+    for i in range(longueur) :
+        if i%2 != 0: #alors nb impaire
+            tab_l = ["#"," "*longueur,"#"]
+        else :
+            gauche = longueur
+            gauche -= randint(1,8)
+            droite = longueur-1 - gauche
+            tab_l = ["#",gauche*"#"," ",droite*"#","#"]
+        tab.append(tab_l)
+    tab.append(["#"*(longueur+2)])
+    affiche = ""
+    for i in tab :
+        for z in i :
+            affiche += z
+        affiche += "\n"
+    return affiche
+        
+print(tkt(10,10))
